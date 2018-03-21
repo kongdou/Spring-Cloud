3e89e21c8cc23e61cf4859c397f788c5a6da367c
kongdou <zhao.xiao.jie@outlook.com>
2018-03-21 11:38:03 +0800

    Spring Boot Rest
    Spring Boot Rest

---
+++ spring-boot-rest/src/fun/deepsky/data/rest/DataApplication.java
@@ -0,0 +1,13 @@
+package fun.deepsky.data.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(DataApplication.class);
+		app.run(args);
+	}
+}
